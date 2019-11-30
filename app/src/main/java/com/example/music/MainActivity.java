63cbb1b7cc77bfcce9ca961294402a01b1930ea3
lihang <1670678504@qq.com>
2019-11-30 11:26:36 +0800

    扫描本地音乐资源并显示

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.music;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView mListView;
+    List<Song> list;
+    MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
+    }
+
+    private void initView() {
+        mListView = (ListView) findViewById(R.id.main_listview);
+        list = new ArrayList<>();
+        //把扫描到的音乐赋值给list
+        list = MusicUtils.getMusicData(this);
+        adapter = new MyAdapter(this,list);
+        mListView.setAdapter(adapter);
     }
 }
+
